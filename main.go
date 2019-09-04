c46d1b443f2242c6314ffccb444a5ddf9038f025
kuaner <kuaner@gmail.com>
2019-09-04 16:58:11 +0800

    fix flag description

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 
 func init() {
 	flag.StringVar(&config.token, "ybot-token", "", "Telegram bot token")
-	flag.StringVar(&config.domain, "ybot-domain", "", "Y2bot domain")
+	flag.StringVar(&config.domain, "ybot-domain", "", "Telegram webhook domain")
 	flag.StringVar(&config.outputPath, "ybot-dir", "./", "Specify the output path")
 	flag.IntVar(&config.threadNumber, "ybot-thread", 2, "The number of process thread")
 	flag.IntVar(&config.prot, "ybot-port", 8008, "The port number of server")
-	flag.BoolVar(&config.acme, "ybot-acme", true, "Print extracted data")
-	flag.BoolVar(&config.hook, "ybot-hook", true, "webhook or pull")
-	flag.StringVar(&config.mail, "ybot-mail", "", "Specify the output path")
+	flag.BoolVar(&config.acme, "ybot-acme", true, "Enable autocert")
+	flag.StringVar(&config.mail, "ybot-mail", "", "Specify acme mail address")
+	flag.BoolVar(&config.hook, "ybot-hook", true, "Enable Webhook mode")
 }
 
 func main() {
